6276a5a05c1d3fbff709bc8eb8ff79ff9bb28ec5
duybinhngo <duybinhngo2901@gmail.com>
2023-01-31 16:59:32 +0700

    Initialize project.

---
+++ src/main/java/com/backend/swp/apalary/ApalaryApplication.java
@@ -0,0 +1,13 @@
+package com.backend.swp.apalary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApalaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApalaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/swp/apalary/ApalaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.swp.apalary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApalaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
