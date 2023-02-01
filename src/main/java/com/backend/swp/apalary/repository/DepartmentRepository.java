836a14321e9e46728e602d3041ca63d4a57c96aa
duybinhngo <duybinhngo2901@gmail.com>
2023-02-01 20:57:25 +0700

    Add @Repository annotation to repositories for job offering api.

--- src/main/java/com/backend/swp/apalary/repository/DepartmentRepository.java
+++ src/main/java/com/backend/swp/apalary/repository/DepartmentRepository.java
@@ -2,6 +2,9 @@ package com.backend.swp.apalary.repository;
 
 import com.backend.swp.apalary.model.entity.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+
+@Repository
 public interface DepartmentRepository extends JpaRepository<Department, Integer> {
 }
