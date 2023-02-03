9758f5602173cace1e7a80397efcacbd6acd8584
duybinhngo <duybinhngo2901@gmail.com>
2023-02-04 00:43:46 +0700

    Add method in repositories for job offering api.

--- src/main/java/com/backend/swp/apalary/repository/JobOfferingRepository.java
+++ src/main/java/com/backend/swp/apalary/repository/JobOfferingRepository.java
@@ -1,9 +1,14 @@
 package com.backend.swp.apalary.repository;
 
+import com.backend.swp.apalary.model.constant.Status;
 import com.backend.swp.apalary.model.entity.JobOffering;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface JobOfferingRepository extends JpaRepository<JobOffering, Integer> {
+    List<JobOffering> findJobOfferingByStatus(Status status);
+    JobOffering findJobOfferingByIdAndStatus(Integer id, Status status);
 }
