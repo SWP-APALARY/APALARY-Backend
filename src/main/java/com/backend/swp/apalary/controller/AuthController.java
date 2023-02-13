88bae5495c434b5767219bf6603cccecdc154cea
duybinhngo <duybinhngo2901@gmail.com>
2023-02-13 15:56:47 +0700

    Add notation RequestBody.

--- src/main/java/com/backend/swp/apalary/controller/AuthController.java
+++ src/main/java/com/backend/swp/apalary/controller/AuthController.java
@@ -7,6 +7,7 @@ import com.backend.swp.apalary.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,16 +17,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
     private final AuthService authService;
     @PostMapping("/create/employee")
-    public ResponseEntity<String> createEmployee(CreateEmployeeRequest request) {
+    public ResponseEntity<String> createEmployee(@RequestBody CreateEmployeeRequest request) {
         return authService.createEmployee(request);
     }
     @PostMapping("/create/resident")
-    public ResponseEntity<String> createResident(CreateResidentRequest request) {
+    public ResponseEntity<String> createResident(@RequestBody CreateResidentRequest request) {
         return authService.createResident(request);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(LoginRequest request) {
+    public ResponseEntity<String> login(@RequestBody LoginRequest request) {
+        System.out.println(request);
         return authService.login(request);
     }
 
