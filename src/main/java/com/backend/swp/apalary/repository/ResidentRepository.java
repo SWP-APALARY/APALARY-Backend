51d24d4c866e7a138c0ebf2c517b4af855eaef66
duybinhngo <duybinhngo2901@gmail.com>
2023-02-20 16:27:35 +0700

    Change method to find only active user.

--- src/main/java/com/backend/swp/apalary/repository/ResidentRepository.java
+++ src/main/java/com/backend/swp/apalary/repository/ResidentRepository.java
@@ -1,11 +1,12 @@
 package com.backend.swp.apalary.repository;
 
+import com.backend.swp.apalary.model.constant.Status;
 import com.backend.swp.apalary.model.entity.Resident;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ResidentRepository extends JpaRepository<Resident, String> {
-    Resident findResidentByUsername(String username);
+    Resident findResidentByUsernameAndStatus(String username, Status status);
     boolean existsById(String id);
 }
