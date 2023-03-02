a8838b34d403ee025ddbe6c9fc815e61a7d6de9b
duybinhngo <duybinhngo2901@gmail.com>
2023-03-03 03:55:48 +0700

    Add method to find employee by contract.

--- src/main/java/com/backend/swp/apalary/repository/EmployeeRepository.java
+++ src/main/java/com/backend/swp/apalary/repository/EmployeeRepository.java
@@ -1,6 +1,7 @@
 package com.backend.swp.apalary.repository;
 
 import com.backend.swp.apalary.model.constant.Status;
+import com.backend.swp.apalary.model.entity.Contract;
 import com.backend.swp.apalary.model.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,5 +11,6 @@ import org.springframework.stereotype.Repository;
 public interface EmployeeRepository extends JpaRepository<Employee, String> {
     Employee findEmployeeByIdAndStatus(String id, Status status);
     Employee findEmployeeByUsernameAndStatus(String username, Status status);
+    Employee findEmployeeByContract(Contract contract);
     boolean existsById(String id);
 }
