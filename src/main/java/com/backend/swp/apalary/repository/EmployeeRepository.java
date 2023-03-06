288691499fc3320b78664b53fc348be3274717c7
duybinhngo <duybinhngo2901@gmail.com>
2023-03-06 17:42:58 +0700

    Change method get all Employee.

--- src/main/java/com/backend/swp/apalary/repository/EmployeeRepository.java
+++ src/main/java/com/backend/swp/apalary/repository/EmployeeRepository.java
@@ -6,11 +6,14 @@ import com.backend.swp.apalary.model.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, String> {
     Employee findEmployeeByIdAndStatus(String id, Status status);
     Employee findEmployeeByUsernameAndStatus(String username, Status status);
+    List<Employee> findEmployeeByStatus(Status status);
     Employee findEmployeeByContract(Contract contract);
     boolean existsById(String id);
 }
